b270ffe8068defebfecd80bfe9268f0cafde45ae
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-04-22 21:30:10 +0300

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import TodoCreate from './components/TodoCreate'
 import TodoList from './components/TodoList'
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (newTodo) => {
+    setTodos([...todos, newTodo])
+  }
   return (
     <div className='App'>
       <div className='todoApp'>
-        <TodoCreate />
+        <TodoCreate onCreateTodo={createTodo} />
         <TodoList />
       </div>
     </div>
